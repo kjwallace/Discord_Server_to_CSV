a5c167d81188fcb0607effa7815ce29f58256831
chiatzen <45086907+ChiatzenW@users.noreply.github.com>
2022-12-04 20:11:15 -0500

    deleted my old token

--- Cache_Bot/config.py
+++ Cache_Bot/config.py
@@ -2,7 +2,7 @@ import os
 
 def Config():
     config={
-            "token":'MTA0OTA1NDQ1MDg5NzE0MTg3MQ.GTQOza.kx9hxl15Wt8BZa4ptIwoshYDJOuDmGAPl1e6pA',
+            "token":'your_token_here',
             "command_pref":"!!",
             "permissions":8,
             "output_url" : f'{os.path.dirname(os.path.realpath(__name__))}/Output_CSVs'
