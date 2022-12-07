94205164319f6d08439f92a44b5149d8cbc2251a
Krishanth Suthaharan <krishanth2001@live.com>
2022-12-06 20:11:50 -0500

    Restored original configuration file after testing

--- Cache_Bot/config.py
+++ Cache_Bot/config.py
@@ -2,7 +2,7 @@ import os
 
 def Config():
     config={
-            "token":'MTA0OTUxMTI4NDc3NDgwNTYwNQ.GTbqOC.K9o53DhOTVg28hdp5JpmU5CjUqbkrrPdGrlGvo',
+            "token":'your_token_here',
             "command_pref":"!!",
             "permissions":8,
             "output_url" : f'{os.path.dirname(os.path.realpath(__name__))}/Output_CSVs'
