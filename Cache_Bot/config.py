ec14e038973c2f4ab2e27ba76fd6a9e3aaafbaf6
kjwallace <72354709+kjwallace@users.noreply.github.com>
2023-02-27 12:53:00 -0500

    Update config.py

--- Cache_Bot/config.py
+++ Cache_Bot/config.py
@@ -2,7 +2,7 @@ import os
 
 def Config():
     config={
-            "token":'MTA1NjMwMDYyOTEyMDc5ODc2MA.GOnaoV.a3F9gcnSXzWvaNMVC732J5V5oz89u01Y28PPFE',
+            "token":'your token here',
             "command_pref":"!!",
             "permissions":8,
             "output_url" : f'{os.path.dirname(os.path.realpath(__name__))}/Output_CSVs'
